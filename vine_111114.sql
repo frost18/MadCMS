e5885cee28a43ad7188f0755fe0872eb17501939
Frost <sergei.lukonin@gmail.com>
2014-11-11 18:22:33 +0600

    Database changes

--- vine_111114.sql
+++ vine_111114.sql
@@ -23,20 +23,34 @@ CREATE TABLE IF NOT EXISTS `catalog` (
   KEY `status` (`status`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-#
-# Data for table "catalog"
-#
-
+-- Дамп данных таблицы vine.catalog: 4 rows
 /*!40000 ALTER TABLE `catalog` DISABLE KEYS */;
-INSERT INTO `catalog` VALUES (1,0,'0000-00-00 00:00:00','sdfsdf','active'),(2,0,'0000-00-00 00:00:00','sdfsdfas','active'),(3,0,'0000-00-00 00:00:00','dfsdfgsdfg','active'),(4,0,'0000-00-00 00:00:00','sdfgsdfgsdf','active');
+REPLACE INTO `catalog` (`id`, `page_id`, `date`, `name`, `status`) VALUES
+	(1, 0, '0000-00-00 00:00:00', 'sdfsdf', 'active'),
+	(2, 0, '0000-00-00 00:00:00', 'sdfsdfas', 'active'),
+	(3, 0, '0000-00-00 00:00:00', 'dfsdfgsdfg', 'active'),
+	(4, 0, '0000-00-00 00:00:00', 'sdfgsdfgsdf', 'active');
 /*!40000 ALTER TABLE `catalog` ENABLE KEYS */;
 
-#
-# Structure for table "category"
-#
 
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
+-- Дамп структуры для таблица vine.category
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы vine.category: 3 rows
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+REPLACE INTO `category` (`id`, `parent_id`) VALUES
+	(22, 0),
+	(28, 0),
+	(36, 0);
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица vine.fields
+CREATE TABLE IF NOT EXISTS `fields` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL DEFAULT '0',
   `module` varchar(50) NOT NULL,
@@ -59,12 +73,8 @@ CREATE TABLE `category` (
 /*!40000 ALTER TABLE `fields` ENABLE KEYS */;
 
 
-#
-# Structure for table "goods"
-#
-
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods` (
+-- Дамп структуры для таблица vine.goods
+CREATE TABLE IF NOT EXISTS `goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `catalog_id` int(11) DEFAULT NULL,
   `marker` enum('hot,','bestprice') DEFAULT NULL,
@@ -76,39 +86,42 @@ CREATE TABLE `goods` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-#
-# Data for table "goods"
-#
-
+-- Дамп данных таблицы vine.goods: 3 rows
 /*!40000 ALTER TABLE `goods` DISABLE KEYS */;
-INSERT INTO `goods` VALUES (1,NULL,NULL,NULL,123,NULL,'dfgd','1'),(2,NULL,NULL,NULL,1231,NULL,'dfgsdfg','1'),(3,NULL,NULL,NULL,123,NULL,'sdfg','2');
+REPLACE INTO `goods` (`id`, `catalog_id`, `marker`, `marker_color`, `price`, `old_price`, `name`, `category_id`) VALUES
+	(1, NULL, NULL, NULL, 123, NULL, 'dfgd', '1'),
+	(2, NULL, NULL, NULL, 1231, NULL, 'dfgsdfg', '1'),
+	(3, NULL, NULL, NULL, 123, NULL, 'sdfg', '2');
 /*!40000 ALTER TABLE `goods` ENABLE KEYS */;
 
-#
-# Structure for table "lang"
-#
 
-DROP TABLE IF EXISTS `lang`;
-CREATE TABLE `lang` (
+-- Дамп структуры для таблица vine.lang
+CREATE TABLE IF NOT EXISTS `lang` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) DEFAULT NULL,
-  `value` text,
-  `pid` int(11) DEFAULT NULL,
+  `val` text,
+  `parent_id` int(11) DEFAULT NULL,
   `module` varchar(255) DEFAULT 'site',
   `ln` varchar(3) DEFAULT 'ru',
-  PRIMARY KEY (`Id`),
+  PRIMARY KEY (`id`),
   KEY `key` (`key`),
-  KEY `pid` (`pid`),
+  KEY `pid` (`parent_id`),
   KEY `module` (`module`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы vine.lang: 4 rows
+-- Дамп данных таблицы vine.lang: 10 rows
 /*!40000 ALTER TABLE `lang` DISABLE KEYS */;
-REPLACE INTO `lang` (`Id`, `key`, `value`, `pid`, `module`, `ln`) VALUES
-	(1, 'title', 'О компании', 1, 'pages', 'ru'),
-	(2, 'text', 'Есть несколько подводных камней, которых следует остерегаться. До версии PHP 5.1.0, instanceof вызывал __autoload() если имя класса не существовало. Вдобавок, если класс не был загружен, происходила фатальная ошибка. Это можно было обойти с помощью динамической ссылки на класс или использования строковой переменной с именем класса:', 1, 'pages', 'ru'),
-	(3, 'title', 'About', 1, 'pages', 'en'),
-	(4, 'text', 'There are some pitfalls to watch out for. Prior to version PHP 5.1.0, instanceof caused __autoload () if the class name does not exist. In addition, if the class was not loaded, a fatal error occurs. This can be circumvented by using a dynamic reference to a class or using a string variable with the name of the class:', 1, 'pages', 'en');
+REPLACE INTO `lang` (`id`, `key`, `val`, `parent_id`, `module`, `ln`) VALUES
+	(1, 'title', 'О компании', 1, 'pages', 'rus'),
+	(2, 'text', 'Есть несколько подводных камней, которых следует остерегаться. До версии PHP 5.1.0, instanceof вызывал __autoload() если имя класса не существовало. Вдобавок, если класс не был загружен, происходила фатальная ошибка. Это можно было обойти с помощью динамической ссылки на класс или использования строковой переменной с именем класса:', 1, 'pages', 'rus'),
+	(3, 'title', 'About', 1, 'pages', 'eng'),
+	(4, 'text', 'There are some pitfalls to watch out for. Prior to version PHP 5.1.0, instanceof caused __autoload () if the class name does not exist. In addition, if the class was not loaded, a fatal error occurs. This can be circumvented by using a dynamic reference to a class or using a string variable with the name of the class:', 1, 'pages', 'eng'),
+	(5, 'title', 'saa', 2, 'pages', 'rus'),
+	(6, 'text', 'ad', 2, 'pages', 'rus'),
+	(7, 'title', 'asd', 2, 'pages', 'eng'),
+	(8, 'text', 'asd', 2, 'pages', 'eng'),
+	(9, 'keyw', 'qqwerqwe', 1, 'pages', 'rus'),
+	(10, 'desc', 'qwerqwer', 1, 'pages', 'rus');
 /*!40000 ALTER TABLE `lang` ENABLE KEYS */;
 
 
@@ -117,10 +130,9 @@ CREATE TABLE IF NOT EXISTS `menus` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `position` varchar(50) NOT NULL,
-  `pages` text NOT NULL,
-  `status` enum('active','inactive') DEFAULT 'active',
+  `page_id` int(11) NOT NULL DEFAULT '0',
+  `parent_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `status` (`status`),
   KEY `position` (`position`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -130,29 +142,33 @@ CREATE TABLE IF NOT EXISTS `menus` (
 
 
 -- Дамп структуры для таблица vine.pages
-#
-
-DROP TABLE IF EXISTS `pages`;
-CREATE TABLE `pages` (
+CREATE TABLE IF NOT EXISTS `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pid` int(11) NOT NULL DEFAULT '0',
+  `parent_id` int(11) DEFAULT NULL,
   `module` varchar(255) DEFAULT NULL,
-  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `status` enum('active','inactive') DEFAULT NULL,
   `alias` varchar(255) DEFAULT NULL,
-  `date` datetime NOT NULL,
-  `edit_date` datetime NOT NULL,
-  PRIMARY KEY (`Id`),
+  `date` datetime DEFAULT NULL,
+  `edit_date` datetime DEFAULT NULL,
+  `ln` enum('en','ru') DEFAULT 'ru',
+  `readonly` int(11) DEFAULT '0',
+  `order` int(11) DEFAULT '0',
+  `nofollow` int(11) DEFAULT '0',
+  `new_window` int(11) DEFAULT '0',
+  `custom_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `alias` (`alias`),
-  KEY `pid` (`pid`),
+  KEY `pid` (`parent_id`),
   KEY `visible` (`status`),
   KEY `date` (`date`),
   KEY `edit_date` (`edit_date`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы vine.pages: 1 rows
+-- Дамп данных таблицы vine.pages: 2 rows
 /*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-REPLACE INTO `pages` (`Id`, `pid`, `module`, `status`, `alias`, `date`, `edit_date`) VALUES
-	(1, 0, 'page', 'active', 'about', '2014-09-04 15:35:00', '2014-09-04 17:31:00');
+REPLACE INTO `pages` (`id`, `parent_id`, `module`, `status`, `alias`, `date`, `edit_date`, `ln`, `readonly`, `order`, `nofollow`, `new_window`, `custom_url`) VALUES
+	(1, 0, 'text', 'active', 'about', '2014-09-04 15:35:00', '2014-09-04 17:31:00', '', 0, 0, 0, 0, NULL),
+	(2, 0, 'text', 'active', 'xi', '2014-09-04 15:35:00', '2014-09-04 17:31:00', '', 0, 0, 0, 0, NULL);
 /*!40000 ALTER TABLE `pages` ENABLE KEYS */;
 
 
@@ -173,8 +189,9 @@ CREATE TABLE IF NOT EXISTS `photos` (
 /*!40000 ALTER TABLE `photos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `photos` ENABLE KEYS */;
 
-DROP TABLE IF EXISTS `properties`;
-CREATE TABLE `properties` (
+
+-- Дамп структуры для таблица vine.properties
+CREATE TABLE IF NOT EXISTS `properties` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL DEFAULT '0',
   `module` varchar(50) NOT NULL,
@@ -192,6 +209,11 @@ CREATE TABLE `properties` (
   KEY `status` (`status`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- Дамп данных таблицы vine.properties: 0 rows
+/*!40000 ALTER TABLE `properties` DISABLE KEYS */;
+/*!40000 ALTER TABLE `properties` ENABLE KEYS */;
+
+
 -- Дамп структуры для таблица vine.users
 CREATE TABLE IF NOT EXISTS `users` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
@@ -204,12 +226,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `salt` (`salt`),
   KEY `email` (`email`),
   KEY `role` (`role`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- Дамп данных таблицы vine.users: 1 rows
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 REPLACE INTO `users` (`Id`, `email`, `fio`, `pass`, `salt`, `role`) VALUES
-	(5, 'user2@user.ru', 'Сергей', '60a6a7d2c719c0048eba39be2f161cc6', '54212f3e64abd', NULL);
+	(1, 'user@user.ru', 'User U', 'user', 'salt', 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
